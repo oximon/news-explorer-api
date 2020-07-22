5070a3ec1157cb913e02fe814b8430a05591415e
Polyakov Semion <s_Qin4ik@live.ru>
2020-07-22 14:25:08 +0400

    some fix

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const corsOptions = {
   credentials: true,
 };
 
-// app.use(cors(corsOptions));
+app.use(cors(corsOptions));
 app.use(helmet());
 app.use(cookieParser());
 app.use(bodyParser.json());
